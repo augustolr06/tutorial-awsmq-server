061df7ca7b369ad890a5463848d85519c6934475
Augusto Lazaro <augusto.lr06@gmail.com>
2023-08-29 08:30:52 -0300

    refactor: change the way to connect to aws

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,49 @@
 import amqp from "amqplib";
 import dotenv from "dotenv";
 
+const queue = "queue_test_1";
+
 dotenv.config();
 
 async function connect() {
   try {
-    const connection = await amqp.connect({
-      protocol: "amqp",
-      hostname: process.env.AMAZON_MQ_ENDPOINT,
-      port: parseInt(process.env.AMAZON_MQ_PORT),
-      username: process.env.AMAZON_MQ_USERNAME,
-      password: process.env.AMAZON_MQ_PASSWORD,
-    });
-
-    const channel = await connection.createChannel();
-
-    const queueName = "my_queue";
-    await channel.assertQueue(queueName, { durable: false });
-
-    console.log("Waiting for messages...");
-    channel.consume(
-      queueName,
-      (msg) => {
-        msg && console.log(`Received message: ${msg.content.toString()}`);
+    amqp.connect(
+      {
+        hostname: process.env.AMAZON_MQ_ENDPOINT,
+        port: parseInt(process.env.AMAZON_MQ_PORT),
+        username: process.env.AMAZON_MQ_USERNAME,
+        password: process.env.AMAZON_MQ_PASSWORD,
       },
-      { noAck: true }
+      (err: any, conn: any) => {
+        if (err) throw err;
+
+        // Listener
+        conn.createChannel((err: any, ch2: any) => {
+          if (err) throw err;
+
+          ch2.assertQueue(queue);
+
+          ch2.consume(queue, (msg: any) => {
+            if (msg !== null) {
+              console.log(msg.content.toString());
+              ch2.ack(msg);
+            } else {
+              console.log("Consumer cancelled by server");
+            }
+          });
+        });
+
+        // Sender
+        conn.createChannel((err: any, ch1: any) => {
+          if (err) throw err;
+
+          ch1.assertQueue(queue);
+
+          setInterval(() => {
+            ch1.sendToQueue(queue, Buffer.from("something to do"));
+          }, 1000);
+        });
+      }
     );
   } catch (error) {
     console.error("Error:", error);
